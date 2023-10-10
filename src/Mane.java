a02210e4e3c571e94cff429c08a7223ed9f32f1c
excelaiyt <excelaiyt12@gmail.com>
2023-10-10 19:09:48 +0300

    add commit

---
+++ src/Mane.java
@@ -0,0 +1,17 @@
+public class Mane {
+    public static void main(String[] args) {
+        double price = 10010;
+        if (price > 1000)
+            price = price * 0.9;
+        System.out.println("Стоимость вашей покупки " + price + " рублей");
+    }
+}
+
+
+
+
+
+
+
+
+
