8f22a921ceaf2d6d870077687b10b84683e0a84d
excelaiyt <excelaiyt12@gmail.com>
2023-10-10 20:42:55 +0300

    add commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public double priceCalculation(double price, int count) {
+        price = 1050;
+        count = 800;
+        return price * count;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
